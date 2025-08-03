570d435fda45411a66010d0fbfd061142271420c
Danil Galas <d.galas@entangle.fi>
2025-08-03 16:29:28 +0700

    fix fmt

--- src/AchievementsHook.sol
+++ src/AchievementsHook.sol
@@ -1,64 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
- 
+
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 import {ERC1155} from "solmate/src/tokens/ERC1155.sol";
- 
+
 import {Currency} from "v4-core/types/Currency.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
- 
+
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
- 
+
 import {Hooks} from "v4-core/libraries/Hooks.sol";
- 
+
 contract AchievementsHook is BaseHook, ERC1155 {
     struct Achievements {
         uint256 weiVolume;
         uint8 achievementsCounter;
     }
 
-    mapping (address => Achievements) public swapVolume;
+    mapping(address => Achievements) public swapVolume;
 
-    constructor(
-        IPoolManager _manager
-    ) BaseHook(_manager) {}
- 
-	// Set up hook permissions to return `true`
-	// for the two hook functions we are using
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: false,
-                beforeAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterAddLiquidity: false,
-                afterRemoveLiquidity: false,
-                beforeSwap: false,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+    constructor(IPoolManager _manager) BaseHook(_manager) {}
+
+    // Set up hook permissions to return `true`
+    // for the two hook functions we are using
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterAddLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
- 
+
     // Implement the ERC1155 `uri` function
     function uri(uint256) public view virtual override returns (string memory) {
         return "https://api.example.com/token/{id}";
     }
- 
-	function _afterSwap(
+
+    function _afterSwap(
         address,
         PoolKey calldata key,
         SwapParams calldata swapParams,
@@ -67,10 +59,10 @@ contract AchievementsHook is BaseHook, ERC1155 {
     ) internal override returns (bytes4, int128) {
         // If this is not an ETH-TOKEN pool with this hook attached, ignore
         if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
-    
+
         // We only mint achievements if user is buying TOKEN with ETH
         if (!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
-    
+
         // Mint achievements equal to 20% of the amount of ETH they spent
         // Since its a zeroForOne swap:
         // if amountSpecified < 0:
@@ -79,30 +71,28 @@ contract AchievementsHook is BaseHook, ERC1155 {
         // if amountSpecified > 0:
         //      this is an "exact output for input" swap
         //      amount of ETH they spent is equal to BalanceDelta.amount0()
-    
+
         uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
-    
+
         // Mint the achievement
         _mintAchievement(ethSpendAmount, hookData);
-    
+
         return (this.afterSwap.selector, 0);
     }
 
     event Debug(uint256 a, uint256 b);
-    function _mintAchievement(
-        uint256 ethSpendAmount,
-        bytes calldata hookData
-    ) internal {
+
+    function _mintAchievement(uint256 ethSpendAmount, bytes calldata hookData) internal {
         // If no hookData is passed in, no achievements will be assigned to anyone
         if (hookData.length == 0) return;
-    
+
         // Extract user address from hookData
         address user = abi.decode(hookData, (address));
-    
+
         // If there is hookData but not in the format we're expecting and user address is zero
         // nobody gets any achievements
         if (user == address(0)) return;
-        
+
         Achievements storage achievements = swapVolume[user];
         achievements.weiVolume += ethSpendAmount;
         uint8 achievementsCounter = achievements.achievementsCounter;
--- test/AchievementsHook.t.sol
+++ test/AchievementsHook.t.sol
@@ -1,82 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
- 
+
 import {Test} from "forge-std/Test.sol";
- 
+
 import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
 import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
- 
+
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
- 
+
 import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
- 
+
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {TickMath} from "v4-core/libraries/TickMath.sol";
 import {SqrtPriceMath} from "v4-core/libraries/SqrtPriceMath.sol";
 import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
- 
+
 import {ERC1155TokenReceiver} from "solmate/src/tokens/ERC1155.sol";
- 
+
 import "forge-std/console.sol";
 import {AchievementsHook} from "../src/AchievementsHook.sol";
- 
+
 contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
-	MockERC20 token; // our token to use in the ETH-TOKEN pool
- 
-	// Native tokens are represented by address(0)
-	Currency ethCurrency = Currency.wrap(address(0));
-	Currency tokenCurrency;
- 
-	AchievementsHook hook;
- 
-	function setUp() public {
-		// Step 1 + 2
+    MockERC20 token; // our token to use in the ETH-TOKEN pool
+
+    // Native tokens are represented by address(0)
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency;
+
+    AchievementsHook hook;
+
+    function setUp() public {
+        // Step 1 + 2
         // Deploy PoolManager and Router contracts
         deployFreshManagerAndRouters();
-    
+
         // Deploy our TOKEN contract
         token = new MockERC20("Test Token", "TEST", 18);
         tokenCurrency = Currency.wrap(address(token));
-    
+
         // Mint a bunch of TOKEN to ourselves and to address(1)
         token.mint(address(this), 10000 ether);
         token.mint(address(1), 10000 ether);
-    
+
         // Deploy hook to an address that has the proper flags set
         uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
         deployCodeTo("AchievementsHook.sol", abi.encode(manager), address(flags));
-    
+
         // Deploy our hook
         hook = AchievementsHook(address(flags));
-    
+
         // Approve our TOKEN for spending on the swap router and modify liquidity router
         // These variables are coming from the `Deployers` contract
         token.approve(address(swapRouter), type(uint256).max);
         token.approve(address(modifyLiquidityRouter), type(uint256).max);
-    
+
         // Initialize a pool
-        (key, ) = initPool(
+        (key,) = initPool(
             ethCurrency, // Currency 0 = ETH
             tokenCurrency, // Currency 1 = TOKEN
             hook, // Hook Contract
             3000, // Swap Fees
             SQRT_PRICE_1_1 // Initial Sqrt(P) value = 1
         );
-    
+
         // Add some liquidity to the pool
         uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
-    
+
         uint256 ethToAdd = 30 ether;
-        uint128 liquidityDelta = LiquidityAmounts.getLiquidityForAmount0(
-            SQRT_PRICE_1_1,
-            sqrtPriceAtTickUpper,
-            ethToAdd
-        );
-    
+        uint128 liquidityDelta = LiquidityAmounts.getLiquidityForAmount0(SQRT_PRICE_1_1, sqrtPriceAtTickUpper, ethToAdd);
+
         modifyLiquidityRouter.modifyLiquidity{value: ethToAdd}(
             key,
             ModifyLiquidityParams({
@@ -87,14 +83,14 @@ contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
             }),
             ZERO_BYTES
         );
-	}
+    }
 
-    function test_swap_1_eth_twice() public {    
+    function test_swap_1_eth_twice() public {
         // Set user address in hook data
         bytes memory hookData = abi.encode(address(this));
-    
+
         // We will swap 1 ether for tokens
-        // We should get 1 achievement for 1 eth swap volume 
+        // We should get 1 achievement for 1 eth swap volume
         swapRouter.swap{value: 1 ether}(
             key,
             SwapParams({
@@ -102,17 +98,11 @@ contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
                 amountSpecified: -1 ether, // Exact input for output swap
                 sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
             }),
-            PoolSwapTest.TestSettings({
-                takeClaims: false,
-                settleUsingBurn: false
-            }),
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
             hookData
         );
 
-        uint256 balanceAfterSwap = hook.balanceOf(
-            address(this),
-            1
-        );
+        uint256 balanceAfterSwap = hook.balanceOf(address(this), 1);
         assertEq(balanceAfterSwap, 1);
 
         (uint256 weiVolume, uint8 counter) = hook.swapVolume(address(this));
@@ -128,23 +118,14 @@ contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
                 amountSpecified: -1 ether, // Exact input for output swap
                 sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
             }),
-            PoolSwapTest.TestSettings({
-                takeClaims: false,
-                settleUsingBurn: false
-            }),
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
             hookData
         );
 
-        balanceAfterSwap = hook.balanceOf(
-            address(this),
-            1
-        );
+        balanceAfterSwap = hook.balanceOf(address(this), 1);
         assertEq(balanceAfterSwap, 1);
 
-        balanceAfterSwap = hook.balanceOf(
-            address(this),
-            2
-        );
+        balanceAfterSwap = hook.balanceOf(address(this), 2);
         assertEq(balanceAfterSwap, 1);
 
         (weiVolume, counter) = hook.swapVolume(address(this));
@@ -152,12 +133,12 @@ contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
         assertEq(counter, 2);
     }
 
-    function test_swap_3_eth() public {    
+    function test_swap_3_eth() public {
         // Set user address in hook data
         bytes memory hookData = abi.encode(address(this));
-    
+
         // We will swap 3 ether for tokens
-        // We should get 3 achievements (for 1, 2 and 3 eth volume reached in total) 
+        // We should get 3 achievements (for 1, 2 and 3 eth volume reached in total)
         swapRouter.swap{value: 3 ether}(
             key,
             SwapParams({
@@ -165,10 +146,7 @@ contract TestAchievementsHook is Test, Deployers, ERC1155TokenReceiver {
                 amountSpecified: -3 ether, // Exact input for output swap
                 sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
             }),
-            PoolSwapTest.TestSettings({
-                takeClaims: false,
-                settleUsingBurn: false
-            }),
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
             hookData
         );
 
